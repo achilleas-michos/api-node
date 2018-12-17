de1cbad025756076f8a48b30266411bab70ad42a
Achilleas Michos <michos.achilleas@gmail.com>
2018-12-17 18:37:29 +0100

    Swagger documentation for the API

--- server/api/routes.ts
+++ server/api/routes.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+import yaml from "yamljs";
 import { datagridRouter } from "./datagrid/index";
 
 const setupRoutes = (app: express.Application) => {
   app.use("/api/", datagridRouter);
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(yaml.load("./api/swagger.yaml")));
 };
 
 export { setupRoutes };
