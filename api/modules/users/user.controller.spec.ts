2c837555310311dfaa644b0edf57088908aa1223
Achilleas Michos <michos.achilleas@gmail.com>
2018-12-18 19:24:27 +0100

    fix for non ending supertest

--- api/modules/users/user.controller.spec.ts
+++ api/modules/users/user.controller.spec.ts
@@ -2,11 +2,11 @@ import supertest from "supertest";
 import { app } from "../../app";
 
 describe("GET /users - list all users", () => {
-    it("Simple listing", () => {
+    it("Simple listing", (done) => {
         supertest(app)
                 .get("/api/users")
                 .set("Accept", "application/json")
-                .expect("Content", "application/json; charset=utf-8")
-                .expect(200);
+                .expect("Content-Type", "application/json; charset=utf-8")
+                .expect(200, done);
     });
 });
